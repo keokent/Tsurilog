1b4824885cca47014eff882b0b6900e76d1ad636
menchikatsu <menchikatsu@paperboy.co.jp>
2013-07-30 09:12:40 +0900

    Added relationship model and db migrating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130729001619) do
+ActiveRecord::Schema.define(version: 20130730000403) do
 
   create_table "lures", force: true do |t|
     t.string   "name"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20130729001619) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "relationships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "lure_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["lure_id"], name: "index_relationships_on_lure_id"
+  add_index "relationships", ["user_id", "lure_id"], name: "index_relationships_on_user_id_and_lure_id", unique: true
+  add_index "relationships", ["user_id"], name: "index_relationships_on_user_id"
 
   create_table "reviews", force: true do |t|
     t.text     "content"
