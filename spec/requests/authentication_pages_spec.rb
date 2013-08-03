0b6589bcebc7a04af77f660dfc674b281989d4f7
keoken <keokenppb@gmail.com>
2013-08-04 04:18:54 +0900

    add authorization

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,4 +36,43 @@ describe "Authentication" do
       end
     end
   end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "visit users#show" do
+        before { visit edit_user_path(user) }
+        it { should have_content('Sign in') }
+      end
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            expect(page).to have_content('edit')
+          end
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "visiting Users page" do
+        before { visit user_path(wrong_user) }
+        it { should_not have_content(user.name) }
+      end
+    end
+  end
 end
