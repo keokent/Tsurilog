f445ab68b488722fa136846a6aa3cad0e85e7857
keoken <keokenppb@gmail.com>
2013-08-03 17:22:58 +0900

    add signin form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ TsurilogApp::Application.routes.draw do
   root 'static_pages#home'
   match '/signup', to: 'users#new',  via:'get'
   match '/signin', to: 'sessions#new', via: 'get'
-  match '/siginout', to: 'sessions#destroy', via: 'delete'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/help', to: 'static_pages#help', via:'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
