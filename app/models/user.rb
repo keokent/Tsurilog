bf4cad045f1b3ab8147fd72a8bd3efa27ca8549d
keoken <keokenppb@gmail.com>
2013-08-04 09:56:42 +0900

    showページに登録したルアーを表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_many :reviews, dependent: :destroy
   has_many :relationships
-  has_many :luers, :through => :relationships
+  has_many :lures, :through => :relationships
 
   validates :name, presence: true
   validates :email, presence: true
